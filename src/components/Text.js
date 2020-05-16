7c45ea6675276ad3816a6e0510c5d321255f33cb
mgracesb <mercysbarcel@gmail.com>
2020-05-16 13:03:54 +0200

    edit sass

--- src/components/Text.js
+++ src/components/Text.js
@@ -5,70 +5,45 @@ const Text = (props) => {
   const val = Object.entries(value);
 
   return (
-    <ul>
+    <ul className="infoList">
       {val.map((obj, index) => (
-        <li key={index}>{obj}</li>
+        <li key={index} className={obj[1] !== null ? "li enable" : "li"}>
+          <span
+            className={obj[1] !== null ? `icon ${obj[0]}` : `icon  ${obj[0]}`}
+          ></span>
+
+          <span className={obj[1] !== null ? "yes" : "no"}></span>
+
+          <p className={"infoText"}>
+            {obj[0] === "cc"
+              ? "centros comerciales :"
+              : obj[0] === "takeaway"
+              ? "comida para llevar :"
+              : obj[0] === "enlocal"
+              ? "servicio de mesa :"
+              : obj[0] === "profesional"
+              ? "de alto rendimiento :"
+              : obj[0] === "texto"
+              ? "+ información :"
+              : `${obj[0]} :`}
+            <br />
+
+            {obj[1] === null ? "no permitido" : obj[1]}
+          </p>
+        </li>
       ))}
+
+      <li className={props.other !== "" ? "otherInfo" : "noInfo"}>
+        {props.other !== "" ? props.other : ""}
+      </li>
+      <li className="important">
+        'En todo caso, deberán respetarse las medidas de seguridad e higiene
+        establecidas por las autoridades sanitarias para la prevención del
+        COVID-19, relativas al mantenimiento de una distancia mínima de
+        seguridad de dos metros, higiene de manos y etiqueta respiratoria.'
+      </li>
     </ul>
   );
-  // return (
-  //   <ul className="infoList">
-  //     {val.map((obj, index) => (
-  //       <li
-  //         key={index}
-  //         className={
-  //           obj[0] === "texto"
-  //             ? "li moreInfo"
-  //             : obj[1] !== null
-  //             ? "li enable"
-  //             : "li"
-  //         }
-  //       >
-  //         <span
-  //           className={
-  //             obj[0] === "texto"
-  //               ? "empty"
-  //               : obj[1] !== null
-  //               ? `icon ${obj[0]}`
-  //               : `icon  ${obj[0]}`
-  //           }
-  //         ></span>
-
-  //         <span
-  //           className={
-  //             obj[0] === "texto" ? "empty" : obj[1] !== null ? "yes" : "no"
-  //           }
-  //         ></span>
-
-  //         <p className={obj[0] === "texto" ? "infoText text" : "infoText"}>
-  //           {obj[0] === "cc"
-  //             ? "centros comerciales :"
-  //             : obj[0] === "takeaway"
-  //             ? "comida para llevar :"
-  //             : obj[0] === "enlocal"
-  //             ? "servicio de mesa :"
-  //             : obj[0] === "profesional"
-  //             ? "de alto rendimiento :"
-  //             : obj[0] === "texto"
-  //             ? "+ información :"
-  //             : `${obj[0]} :`}
-  //           <br />
-  //           {obj[1] !== null ? obj[1] : obj[1] === null ? "no" : obj[1]}
-  //         </p>
-  //       </li>
-  //     ))}
-
-  //     <li className={props.other !== "" ? "otherInfo" : "noInfo"}>
-  //       {props.other !== "" ? props.other : ""}
-  //     </li>
-  //     <li className="important">
-  //       'En todo caso, deberán respetarse las medidas de seguridad e higiene
-  //       establecidas por las autoridades sanitarias para la prevención del
-  //       COVID-19, relativas al mantenimiento de una distancia mínima de
-  //       seguridad de dos metros, higiene de manos y etiqueta respiratoria.'
-  //     </li>
-  //   </ul>
-  // );
 };
 
 export default Text;
--- src/services/data.js
+++ src/services/data.js
@@ -7,26 +7,23 @@ const phases = [
         bodas: null,
       },
       comercios: {
-        pequeños: [
-          "si",
+        pequeños:
           "Apertura de comercios de menos de 400m² con cita previa y atención individual.",
-        ],
         grandes: null,
         mercadillos: null,
         cc: null,
       },
       hostelería: {
-        takeaway: [
-          "si",
+        takeaway:
           "Apertura de pequeños locales: atención  individual y con cita previa.",
-        ],
+
         enlocal: null,
         terrazas: null,
         discotecas: null,
         hoteles: null,
       },
       deporte: {
-        individual: ["si", "Sin abandonar el municipio de residencia"],
+        individual: "Sin abandonar el municipio de residencia",
         profesional: null,
         competiciones: null,
       },
@@ -48,69 +45,58 @@ const phases = [
     name: "uno",
     actividades: {
       ceremonias: {
-        cultos: ["si", "Aforo limitado al 33%."],
+        cultos: "Aforo limitado al 33%.",
         bodas: null,
       },
       comercios: {
-        pequeños: [
-          "si",
-          "Apertura de comercios de menos de 400m², que no se encuentren dentro de parques o centros comerciales sin acceso directo e independiente desde el exterior. Mercadillos: aforo limitado al 25%.",
-        ],
-        grandes: null,
+        pequeños:
+          "Apertura de comercios de menos de 400m², que no se encuentren dentro de parques o centros comerciales sin acceso directo e independiente desde el exterior.",
         mercadillos: [
-          "si",
           "Apertura de mercadillos con funcionamiento hasta el 25% y aforo limitado al 30%.",
           "Prohibido el autoservicio y los productos de prueba.",
         ],
+        grandes: null,
         cc: null,
       },
       hostelería: {
-        takeaway: ["si", "Con cita previa"],
+        takeaway: "Con cita previa",
         enlocal: null,
-        terrazas: [
-          "si",
+        terrazas:
           "Apertura de terrazas con ciertas limitaciones: aforo hasta 30%-50%",
-        ],
+
         discotecas: null,
-        hoteles: [
-          "si",
+        hoteles:
           "No se permite el uso de instalaciones de uso común, como piscinas, gimnasios o zonas de recreo.",
-        ],
       },
       deporte: {
-        individual: [
-          "si",
+        individual:
           "Se podrán realizar actividades que no impliquen contacto físico ni uso de vestuarios",
-        ],
-        profesional: [
-          "si",
-          "Apertura de centros deportivos de alto rendimiento.",
-        ],
+
+        profesional: "Apertura de centros deportivos de alto rendimiento.",
+
         competiciones: null,
       },
       cultura: {
-        museos: ["si", "Apertura de museos: aforo limitado al 33%."],
-        bibliotecas: "si",
+        museos: "Apertura de museos: aforo limitado al 33%.",
+        bibliotecas:
+          "Apertura de bibliotecas para préstamo y devolución de obras, lectura en sala y para información bibliográfica y bibliotecaria",
       },
       ocio: {
         cine: null,
         espectáculos: [
-          "si",
           "Actividades culturales: aforo limitado al 33%",
           "Lugares cerrados: máximo 30 personas",
           "Al aire libre: máximo 200 personas sentadas.",
         ],
+
         exteriores: [
-          "si",
           "Actividades culturales: aforo limitado al 33%",
           "Al aire libre: máximo 200 personas sentadas.",
         ],
       },
       funerales: {
-        funerales: [
-          "si",
+        funerales:
           "Velatorios y actos fúnebres: aforo limitado de 15 personas en espacios al aire libre y de 10 personas en espacios cerrados.",
-        ],
       },
     },
   },
@@ -118,44 +104,41 @@ const phases = [
     name: "dos",
     actividades: {
       ceremonias: {
-        cultos: ["si", "aforo limitado al 50%."],
+        cultos: "aforo limitado al 50%.",
         bodas: "si",
       },
       comercios: {
         pequeños: "si",
         grandes: "si",
-        mercadillos: "si",
-        cc: "si",
-        texto:
-          "Mercadillos: aforo limitado al 33%. Apertura de centros comerciales, salvo zonas comunes o áreas recreativas: aforo limitado al 40%.",
+        mercadillos: "Mercadillos: aforo limitado al 33%.",
+        cc:
+          "Apertura de centros comerciales, salvo zonas comunes o áreas recreativas: aforo limitado al 40%.",
       },
       hostelería: {
         takeaway: "si",
-        enlocal: "si",
+        enlocal:
+          "Apertura de restaurantes y bares para el servicio de mesas, aforo limitado al 33%",
         terrazas: "si",
         discotecas: null,
-        hoteles: "si",
-        texto:
-          "Apertura de restaurantes y bares para el servicio de mesas, aforo limitado al 33% y respetando la distancia mínima recomendada. Apertura de zonas comunes en hoteles con restricciones específicas.",
+        hoteles:
+          " Apertura de zonas comunes en hoteles con restricciones específicas",
       },
       deporte: {
         individual: "si",
         profesional: "si",
-        competiciones: "si",
-        texto:
-          "Campeonatos profesionales en partidos a puerta cerrada o con aforo limitado, con posibilidad de ser retransmitidos.",
+        competiciones:
+          "Campeonatos profesionales en partidos a puerta cerrada o con aforo limitado y con posibilidad de ser retransmitidos.",
       },
       cultura: {
         museos: "si",
         bibliotecas: "si",
-        texto: "Apertura de bibliotecas limitado al servicio de préstamo.",
       },
       ocio: {
-        cine: "si",
-        espectáculos: "si",
-        exteriores: "si",
-        texto:
-          "Apertura de cines y teatros: aforo limitado al 50%. Actividades culturales en lugares cerrados: máximo 50 personas, y al aire libre: máximo 400 personas sentadas.",
+        cine: "Apertura de cines y teatros: aforo limitado al 50%.",
+        espectáculos:
+          "Actividades culturales en lugares cerrados: máximo 50 personas y, al aire libre: máximo 400 personas sentadas.",
+        exteriores:
+          " Actividades culturales al aire libre: máximo 400 personas sentadas",
       },
       funerales: {
         funerales: "si",
@@ -172,19 +155,18 @@ const phases = [
       comercios: {
         pequeños: "si",
         grandes: "si",
-        mercadillos: "si",
-        cc: "si",
-        texto:
-          "Apertura de zonas comunes y recreativas en Centros Comerciales: aforo limitado al 50%. Mercadillo: aforo limitado al 50%.",
+        mercadillos: "Mercadillo: aforo limitado al 50%.",
+        cc:
+          "Apertura de zonas comunes y recreativas en Centros Comerciales: aforo limitado al 50%.",
       },
       hostelería: {
         takeaway: "si",
-        enlocal: "si",
+        enlocal: "Aforo limitado al 50% y consumo en barra.",
         terrazas: "si",
-        discotecas: "si",
-        hoteles: "si",
-        texto:
-          "Restaurantes: aforo limitado al 50% y consumo en barra. Apertura de bares nocturnos y discotecas con aforo limitado al 33%. Apertura de zonas comunes en hoteles y alojamientos turísticos: aforo limitado al 50%.",
+        discotecas:
+          "Apertura de bares nocturnos y discotecas con aforo limitado al 33%.",
+        hoteles:
+          "Apertura de zonas comunes en hoteles y alojamientos turísticos: aforo limitado al 50%.",
       },
       deporte: {
         individual: "si",
@@ -197,9 +179,8 @@ const phases = [
       },
       ocio: {
         cine: "si",
-        espectáculos: "si",
-        exteriores: "si",
-        texto: "Actividades culturales: aumenta el aforo limitado.",
+        espectáculos: "Actividades culturales: aumenta el aforo limitado.",
+        exteriores: "Actividades culturales: aumenta el aforo limitado.",
       },
       funerales: {
         funerales: "si",
